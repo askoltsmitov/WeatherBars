55087b9b23873ab1f86775122335ce65df730dd7
askoltsmitov <sibirskiywolf@mail.ru>
2018-12-04 04:11:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,8 +37,9 @@ async def on_ready():
 
 	channel = bot.get_channel(199459074243297280)
 	await channel.send("Температура на завтра: " + temp + " C" + weather_cry)
+	await logout()
 
-schedule.every().day.at("20:07").do(getRun)
+schedule.every().day.at("15:00").do(getRun)
 
 while True:
 	schedule.run_pending()
